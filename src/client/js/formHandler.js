5b3db523a3318017512c4285dd308218fc9c640e
SalahAmer2 <salahayman2@gmail.com>
2020-03-06 21:27:56 +0200

    second commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,27 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault()
+  let baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+  let apiKey = "49be82b027adf26dca23a428a21e0f92";
+  // check what text was put into the form field
+  let formText = document.getElementById('name').value
+  Client.checkForName(formText)
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+   console.log("::: Form Submitted :::")
+  fetch('http://localhost:8080/test')
+  .then(res => res.json())
+  .then(function(data) {
+      document.getElementById('results').innerHTML = data.message
+  })
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
+  fetch(baseURL + "24701" + ",us&appid=" + apiKey)
+    .then(res => {
+        return res.json()
+    })
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+        document.getElementById('results').innerHTML = data.main.temp
     })
 }
 
-export { handleSubmit }
+export {
+  handleSubmit
+}
